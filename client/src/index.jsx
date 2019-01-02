343d0b3e54bccf1145ea1224f19dacf4d7f4f173
Siege211 <Siege211@morris.umn.edu>
2019-01-02 10:37:02 -0600

    Added lifecycle function to retrieve comments on DidMount. render fetched comments dynamically

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,18 +1,49 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 const styles = {
   app: {
     paddingTop: 40,
     textAlign: 'center',
+    border: '#becde5 1px solid',
+    borderRadius: '25px'
   },
+  parentComment: {
+    display: 'flex',
+    border: '#becde5 1px solid',
+    borderRadius: '25px'
+  }
 }
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {comments: [{dateCreated: '15',user:'shaggy',textContent:'nope'}]};
+  }
+  componentDidMount() {
+    axios.get('/api/comments')
+    .then(response => {
+      console.log("Array of comments: ",response.data);
+      this.setState({comments: response.data});
+    })
+    .catch(err => {
+    console.log(err)
+    })
+  }
+
   render() {
     return (
-      <div style={styles.app}>
-        Welcome to React!
+      <div style={styles.app} id='app'>
+        {this.state.comments.map((comment,i) => {
+          let date = comment.dateCreated;
+        return (
+          <div style={styles.parentComment} key={`comment#${i}`}>
+            <div>{`${date.substring(0,10)}: ${date.substring(11,16)}`}</div>
+            <div>{comment.user}</div>
+            <div>{comment.textContent}</div>
+      </div>
+        )
+      })}
       </div>
     )
   }
@@ -20,3 +51,5 @@ class App extends Component {
 
 const root = document.querySelector('#app')
 ReactDOM.render(<App />, root)
+
+export default App;
---
+++ test/test.js
@@ -0,0 +1,25 @@
+// import React from 'react';
+// import { shallow, mount, render } from 'enzyme';
+// import App from '../client/src/index';
+// import ReactDOM from 'react-dom';
+
+describe('A suite', function() {
+  it('should be able to run tests', () => {
+        expect(1 + 2).toEqual(3);
+    });
+  it('should render without throwing an error', function() {
+    expect(shallow(<App />).contains(<div style={styles.app} id="app">Welcome to React!</div>)).toBe(true);
+  });
+
+  // it('should be selectable by class "header"', function() {
+  //   expect(shallow(<App />).is('.header')).toBe(true);
+  // });
+
+  // it('should mount in a full DOM', function() {
+  //   expect(mount(<App />).find('.header').length).toBe(1);
+  // });
+
+  // it('should render to static HTML', function() {
+  //   expect(render(<App />).text()).toEqual('Welcome to React!');
+  // });
+});
