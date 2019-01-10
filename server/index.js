38b27937ecf895ad9ac53afb9b70108727db7f49
carterwc <cacbcarter68@gmail.com>
2019-01-09 23:11:50 -0500

    creating new branch for SDC component

--- server/index.js
+++ server/index.js
@@ -5,40 +5,41 @@ const app = express();
 const PORT = 3004;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + '/../client/public'));
 
 
 app.get('/api/comments', (req, res) => {
-	db.GetAllComments( 
-		(err,comments) => {
-			if (err) {throw err}
-			else {res.send(comments);}
+	db.GetAllComments(
+		(err, comments) => {
+			if (err) { throw err }
+			else { res.send(comments); }
 		}
 	);
- 
+
 });
 
-app.get('/api/singleComment', (req,res) => {
-	console.log("comment id: ",req.query);
+app.get('/api/singlecomment', (req, res) => {
+	console.log("comment id: ", req.query);
 	db.GetOneComment(req.query.commentId,
-		(err,comment) => {
-			if (err) {throw err}
-			else {res.send(comment)}
+		(err, comment) => {
+			if (err) { throw err }
+			else { res.send(comment) }
 		}
 	);
 })
 
-app.post('/api/comments', (req,res) => {
+app.post('/api/comments', (req, res) => {
 	db.AddOne(req.body,
-		(err,comment) => {
-			if (err) {console.log('error in express');throw err;}
+		(err, comment) => {
+			if (err) { console.log('error in express'); throw err; }
 			else {
 				console.log(comment);
-				res.send(200,comment.insertId)}
-				// ^ Send insertId to client
-				// so that client can automatically add the correct comment
+				res.send(200, comment.insertId)
+			}
+			// ^ Send insertId to client
+			// so that client can automatically add the correct comment
 		}
 	)
 })
@@ -46,5 +47,5 @@ app.post('/api/comments', (req,res) => {
 
 
 app.listen(PORT, () => {
-  console.log(`Server running on Localhost:${PORT}`);
+	console.log(`Server running on Localhost:${PORT}`);
 });
